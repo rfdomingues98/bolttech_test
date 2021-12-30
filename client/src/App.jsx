f707e89fec5c96cdb3cf37e1db381cd51bc0a49b
Ricardo Domingues <rfdomingues98@gmail.com>
2021-12-30 20:35:35 +0000

    Removed useless files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,14 @@ import { ThemeProvider } from '@mui/material/styles';
 import { AuthProvider } from './contexts/auth';
 import Routes from './routes';
 import { theme } from './config/theme';
-import './App.css';
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </AuthProvider>
   </ThemeProvider>
 );
 
